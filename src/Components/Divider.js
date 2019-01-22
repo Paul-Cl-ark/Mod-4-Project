ae8d8b60a3ce695c5e191156497f693cc25cc6c0
Paul-Cl-ark <clark.paul123@gmail.com>
2019-01-22 13:58:51 +0000

    refactors login form into divider form

--- src/Components/Divider.js
+++ src/Components/Divider.js
@@ -1,25 +1,70 @@
 import React from 'react'
 import { Button, Divider, Form, Grid, Segment } from 'semantic-ui-react'
 
-const DividerExampleVerticalForm = () => (
-  <Segment placeholder>
-    <Grid columns={2} relaxed='very' stackable>
-      <Grid.Column>
-        <Form>
-          <Form.Input icon='envelope outline' iconPosition='left' label='Email' placeholder='Email' />
-          <Form.Input icon='lock' iconPosition='left' label='Password' type='password' />
+class DividerExampleVerticalForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
 
-          <Button content='Login' primary />
-        </Form>
-      </Grid.Column>
+  handleChange = (data) => {
+    this.setState({
+      [data.name]: data.value
+    })
+  }
 
-      <Grid.Column verticalAlign='middle'>
-        <Button content='Sign up' icon='signup' size='big' />
-      </Grid.Column>
-    </Grid>
+  handleSubmit = (event) => {
+      event.preventDefault()
+      fetch('http://localhost:3000/api/v1/login', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(
+          {
+            user:
+              {
+                email: this.state.email,
+                password: this.state.password
+              }
+          }
+        )
+      })
+      .then(r => r.json())
+      .then(data => {
+        if (data.error) {
+        alert(data.error)
+      } else {
+        this.props.logIn(data.user, data.token)
+      }
+    })
+  }
 
-    <Divider vertical>Or</Divider>
-  </Segment>
-)
+  render() {
+    return(
+      <Segment placeholder>
+      <Grid columns={2} relaxed='very' stackable>
+        <Grid.Column>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Input icon='envelope outline' iconPosition='left' label='Email' placeholder='Email' name="email" onChange={(event, data) => this.handleChange(data)} />
+            <Form.Input icon='lock' iconPosition='left' label='Password' type='password' name="password" onChange={(event, data) => this.handleChange(data)} />
+
+            <Button content='Login' primary />
+          </Form>
+        </Grid.Column>
+
+        <Grid.Column verticalAlign='middle'>
+          <Button content='Sign up' icon='signup' size='big' />
+        </Grid.Column>
+      </Grid>
+
+      <Divider vertical>Or</Divider>
+    </Segment>
+    )
+  }
+}
 
 export default DividerExampleVerticalForm
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,4 @@
 import React, { Component, Image } from 'react';
-import LogInForm from './LogInForm'
 import RegisterForm from './RegisterForm'
 import Divider from './Divider'
 
@@ -11,7 +10,7 @@ class Home extends Component {
   render() {
 
     const componentToRender = () => {
-    return this.props.user ? <img src='https://media.giphy.com/media/l1ughbsd9qXz2s9SE/giphy.gif' /> : <React.Fragment><LogInForm logIn={this.props.logIn} /> <RegisterForm /> <Divider /> </React.Fragment>
+    return this.props.user ? <img src='https://media.giphy.com/media/l1ughbsd9qXz2s9SE/giphy.gif' /> : <React.Fragment><RegisterForm /> <Divider logIn={this.props.logIn}/> </React.Fragment>
     }
 
 
