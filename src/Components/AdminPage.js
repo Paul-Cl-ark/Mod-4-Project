ac27bb828d479b9f1da8f2afc987f8fd028ef739
paolov1928 <ventura.paolo@gmail.com>
2019-01-25 09:46:50 +0000

    Added a go back home button using Router DOM Link.

--- src/Components/AdminPage.js
+++ src/Components/AdminPage.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Switch, Route, withRouter, Link } from 'react-router-dom';
+import { Button, Divider, Form, Grid, Segment } from 'semantic-ui-react'
+
 
 class AdminPage extends Component {
 
@@ -8,11 +11,15 @@ class AdminPage extends Component {
     return (
       <React.Fragment>
         <h1>The Sam Barker Fanclub Page</h1>
+        <hr></hr>
         <img src="https://flatiron-v3-production.imgix.net/barker-500x.jpg" alt="The one and only"/>
-      </React.Fragment>
+        <hr></hr>
+      <Link to='/'><Button compact color='blue'>Go back home</Button></Link>
+
+  </React.Fragment>
     )
 
   }
 }
 
-export default AdminPage;
+export default withRouter(AdminPage);
